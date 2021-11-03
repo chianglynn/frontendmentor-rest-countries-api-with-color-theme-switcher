fdd4396b9ba508f7107973addef4d592aacfbff0
Chiang Lynn <lynnchiangtw@gmail.com>
2021-11-03 19:09:27 +0800

    Update API version

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,7 @@
-export const API_URL_COUNTRY = 'https://restcountries.eu/rest/v2/name/';
-export const API_URL_CODE = 'https://restcountries.eu/rest/v2/alpha/';
-export const API_URL_REGION = 'https://restcountries.eu/rest/v2/region/';
-export const API_URL_ALLCOUNTRIES = 'https://restcountries.eu/rest/v2/all';
+// https://restcountries.com/
+const REST_COUNTRIES_VERSION = 3.1;
+export const API_URL_COUNTRY = `https://restcountries.eu/rest/v${REST_COUNTRIES_VERSION}/name/`;
+export const API_URL_CODE = `https://restcountries.eu/rest/v${REST_COUNTRIES_VERSION}/alpha/`;
+export const API_URL_REGION = `https://restcountries.eu/rest/v${REST_COUNTRIES_VERSION}/region/`;
+export const API_URL_ALLCOUNTRIES = `https://restcountries.eu/rest/v${REST_COUNTRIES_VERSION}/all`;
 export const RESULTS_PER_PAGE = 20;
